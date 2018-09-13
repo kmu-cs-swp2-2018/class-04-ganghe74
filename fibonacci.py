857a8b65bf9d11a2be470c823b0eacca485f2057
ganghe74 <ganghe74@kookmin.ac.kr>
2018-09-13 10:40:35 +0900

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,26 @@
+import time
+import random
+
+def fibo(n):
+    if n <= 1:
+        return n
+    return fibo(n - 1) + fibo(n - 2)
+
+def iterfibo(n):
+	a = [0,1,1]
+	for i in range(3,n+1):
+		a.append(a[i-2]+a[i-1])
+	return a[n]
+
+while True:
+    nbr = int(input("Enter a number: "))
+    if nbr == -1:
+        break
+    ts = time.time()
+    fibonumber = iterfibo(nbr)
+    ts = time.time() - ts
+    print("IterFibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+    ts = time.time()
+    fibonumber = fibo(nbr)
+    ts = time.time() - ts
+    print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))

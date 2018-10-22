a6fffe3463a1d3f401f8f638973408d401e97701
김강희 <ganghe74@kookmin.ac.kr>
2018-10-23 08:58:26 +0900

    naming

--- Calculator_Main(07-2).py
+++ Calculator_Main(07-2).py
@@ -1,3 +1,4 @@
+#07-2 계산기 코드 개선 및 기능 추가 과제
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtWidgets import QLineEdit, QToolButton
@@ -5,7 +6,7 @@ from PyQt5.QtWidgets import QSizePolicy
 from PyQt5.QtWidgets import QLayout, QGridLayout
 
 from keypad import numPadList, operatorList, constantList, functionList
-import calcFunctions
+from calcDictionaries import *
 
 class Button(QToolButton):
 
@@ -87,29 +88,11 @@ class Calculator(QWidget):
             self.display.setText(result)
         elif key == 'C':
             self.display.clear()
-        elif key == constantList[0]:
-            self.display.setText(self.display.text() + '3.141592')
-        elif key == constantList[1]:
-            self.display.setText(self.display.text() + '3E+8')
-        elif key == constantList[2]:
-            self.display.setText(self.display.text() + '340')
-        elif key == constantList[3]:
-            self.display.setText(self.display.text() + '1.5E+8')
-        elif key == functionList[0]:
+        elif key in constantList:
+            self.display.setText(self.display.text() + consDictionary[key])
+        elif key in functionList:
             n = self.display.text()
-            value = calcFunctions.factorial(n)
-            self.display.setText(str(value))
-        elif key == functionList[1]:
-            n = self.display.text()
-            value = calcFunctions.decToBin(n)
-            self.display.setText(str(value))
-        elif key == functionList[2]:
-            n = self.display.text()
-            value = calcFunctions.binToDec(n)
-            self.display.setText(str(value))
-        elif key == functionList[3]:
-            n = self.display.text()
-            value = calcFunctions.decToRoman(n)
+            value = funcDictionary[key](n)
             self.display.setText(str(value))
         else:
             self.display.setText(self.display.text() + key)

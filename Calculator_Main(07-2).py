dd9862724206200980ca288ea34e4fd0f14f2ae5
김강희 <ganghe74@kookmin.ac.kr>
2018-10-23 09:18:14 +0900

    fix comments

--- Calculator_Main(07-2).py
+++ Calculator_Main(07-2).py
@@ -91,40 +91,38 @@ class Calculator(QWidget):
         elif key in constantList:
             self.display.setText(self.display.text() + consDictionary[key])
 
-        '''
-        elif key == constantList[0]:
-            self.display.setText(self.display.text() + '3.141592')
-        elif key == constantList[1]:
-            self.display.setText(self.display.text() + '3E+8')
-        elif key == constantList[2]:
-            self.display.setText(self.display.text() + '340')
-        elif key == constantList[3]:
-            self.display.setText(self.display.text() + '1.5E+8')
-        '''
+        #elif key == constantList[0]:
+        #    self.display.setText(self.display.text() + '3.141592')
+        #elif key == constantList[1]:
+        #    self.display.setText(self.display.text() + '3E+8')
+        #elif key == constantList[2]:
+        #    self.display.setText(self.display.text() + '340')
+        #elif key == constantList[3]:
+        #    self.display.setText(self.display.text() + '1.5E+8')
+        
         
         elif key in functionList:
             n = self.display.text()
             value = funcDictionary[key](n)
             self.display.setText(str(value))
 
-        '''
-        elif key == functionList[0]:
-            n = self.display.text()
-            value = calcFunctions.factorial(n)
-            self.display.setText(str(value))
-        elif key == functionList[1]:
-            n = self.display.text()
-            value = calcFunctions.decToBin(n)
-            self.display.setText(str(value))
-        elif key == functionList[2]:
-            n = self.display.text()
-            value = calcFunctions.binToDec(n)
-            self.display.setText(str(value))
-        elif key == functionList[3]:
-            n = self.display.text()
-            value = calcFunctions.decToRoman(n)
-            self.display.setText(str(value))
-        '''
+        
+        #elif key == functionList[0]:
+        #    n = self.display.text()
+        #    value = calcFunctions.factorial(n)
+        #    self.display.setText(str(value))
+        #elif key == functionList[1]:
+        #    n = self.display.text()
+        #    value = calcFunctions.decToBin(n)
+        #    self.display.setText(str(value))
+        #elif key == functionList[2]:
+        #    n = self.display.text()
+        #    value = calcFunctions.binToDec(n)
+        #    self.display.setText(str(value))
+        #elif key == functionList[3]:
+        #    n = self.display.text()
+        #    value = calcFunctions.decToRoman(n)
+        #    self.display.setText(str(value))
 
         else:
             self.display.setText(self.display.text() + key)
